1a2108759dacee0ae5c2cdc95b9ad72cb5f0de6e
NIKIT KUMAR <nikitkumar871@gmail.com>
2026-08-24 00:07:49 +0530

    Update auth.contoller.js

--- server/controllers/auth.contoller.js
+++ server/controllers/auth.contoller.js
@@ -28,7 +28,7 @@ export const googleAuth = async (req, res)=>{
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
-        return res.status(200).json(user)
+        return res.status(200).json({ user, token })
 
     } catch (error) {
         console.error('googleAuth error:', error)
